146eecbc415b75b20b4ec3d95e4851ea6be4a9e5
Zahir <zahir.spencer@yahoo.com>
2021-07-29 23:30:28 -0400

    add price

--- contollers/comics.js
+++ contollers/comics.js
@@ -1,5 +1,9 @@
 import { Comic } from '../models/comics.js'
 import { Profile } from '../models/profile.js'
+
+
+
+
 export{
     index,
     create,
@@ -37,17 +41,20 @@ function edit(req, res) {
     res.render('comics/edit',{
       comics,
       
+      
     })
   })
 }
 
 function index(req, res) {
    Comic.find({})
-  .then((comics ,author,released)=>{
+  .then((comics ,author,released,price)=>{
       res.render('comics/index.ejs',{
           comics,
           author,
-          released
+          released,
+          price
+          
           
       })
   })
@@ -60,30 +67,5 @@ function create(req, res) {
   })
 }
   
-// function addToCollection(req, res) {
-//     // Add id of the logged in user to req.body for creating a game for the first time (if it doesn't exist in the database)
-//     req.body.collectedBy = req.user.profile._id
-//     // Look to see if the game already exists in the database
-//     Comic.findOne({ rawgId: req.params.id })
-//     .then(game => {
-//       // If it does, push the user's profile id to game.collectedBy
-//       if (game) {
-//         comic.collectedBy.push(req.user.profile._id)
-//         comic.save()
-//         .then(() => {
-//           res.redirect(`/comics/${req.params.id}`)
-//         })
-//       } else {
-//         // If it doesn't exist in the database, create it!
-//         Comic.create(req.body)
-//         .then(() => {
-//           res.redirect(`/comics/${req.params.id}`)
-//         })
-//       }
-//     })
-//     .catch(err => {
-//       console.log(err)
-//       res.redirect('/')
-//     })
-//   }
+
   
--- models/comics.js
+++ models/comics.js
@@ -8,9 +8,8 @@ export {
 const comicSchema = new Schema({
   name: {type:String, required: true},
   author: {type:String, required: true},
-  rawgId: Number,
+  price:{type:Number},
   released: Date,
-  imageUrl: String,
   collectedBy: [{ type: Schema.Types.ObjectId, ref: "Profile" }],
   // To be filled in later
   reviews: [{type: Schema.Types.ObjectId, ref: "comicReview"}],
